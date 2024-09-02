5ca5612db1fbb69a3f8f72f95eeac5ad0521d893
Zhu Yan Mei <yanmei.zhu@ncsi.com.cn>
2024-09-02 15:39:36 +0800

    Intitial Commit

---
+++ src/main/java/ym/tmp/com/server/spring_boot_local/SpringBootLocalApplication.java
@@ -0,0 +1,13 @@
+package ym.tmp.com.server.spring_boot_local;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLocalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLocalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ym/tmp/com/server/spring_boot_local/SpringBootLocalApplicationTests.java
@@ -0,0 +1,13 @@
+package ym.tmp.com.server.spring_boot_local;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootLocalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
